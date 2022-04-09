56cbad6b22e4bde6c2c0263889a06a5ba2a7fb83
Roman <graff049@gmail.com>
2022-04-09 22:08:15 +0300

    1. add headers to avoid 403 response from server 2. safety create dir for saving images

--- universe_scraper.py
+++ universe_scraper.py
@@ -1,16 +1,25 @@
+import os.path
+import pathlib
+
 import requests
 
 
-def save_image(image_url, file_name):
-    response = requests.get(url=image_url)
+def save_image(image_url, save_path):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"
+    }
+    response = requests.get(url=image_url, headers=headers)
     response.raise_for_status()
 
-    with open(file_name, 'wb') as image:
+    with open(save_path, 'wb') as image:
         image.write(response.content)
 
 
 if __name__ == '__main__':
     image_url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-    file_name = 'hubble_telescope.jpg'
+    filename = 'hubble_telescope.jpg'
+    dir_path = 'images/'
 
-    save_image(image_url=image_url, file_name=file_name)
+    save_path = os.path.join(dir_path, filename)
+    pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True)
+    save_image(image_url=image_url, save_path=save_path)

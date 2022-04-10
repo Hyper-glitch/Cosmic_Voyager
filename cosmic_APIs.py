d5001562595bd065ec4d483e5e9d3fb7471fc2ec
Roman <graff049@gmail.com>
2022-04-10 18:42:46 +0300

    1. extand get_apod method 2. add get_image_extension function 3. move save_images to cosmic apis and out of class

--- cosmic_APIs.py
+++ cosmic_APIs.py
@@ -5,9 +5,6 @@ import requests
 
 
 # add ABC class here, we don't need implement get_json twice or more
-from dotenv import load_dotenv
-
-
 class SpaceXAPI:
     def __init__(self):
         self.base_url = 'https://api.spacexdata.com/v4/'
@@ -40,16 +37,6 @@ class SpaceXAPI:
                 image_urls.extend(images)
         return image_urls
 
-    @staticmethod
-    def save_images(dir_path, images_content, file_extension, image_name):
-
-        for index, image_content in enumerate(images_content):
-            filename = f'{image_name}{index}{file_extension}'
-            save_path = os.path.join(dir_path, filename)
-
-            with open(save_path, 'wb') as image:
-                image.write(image_content)
-
 
 class NasaAPI:
     def __init__(self, token):
@@ -62,24 +49,27 @@ class NasaAPI:
         self.session.headers.update(self.headers)
         self.session.params = {'api_key': token}
 
-    def get_json(self, endpoint) -> dict:
+    def get_json(self, endpoint, params=None) -> dict:
         url = urllib.urljoin(self.base_url, endpoint)
-        response = self.session.get(url=url)
+        response = self.session.get(url=url, params=params)
         response.raise_for_status()
         return response.json()
 
-    def get_apod(self):
+    def get_apod(self, count):
         """
         APOD it's an Astronomy Picture of the Day
         :return:
         """
         endpoint = 'apod'
-        return self.get_json(endpoint=endpoint)
+        images = self.get_json(endpoint=endpoint, params={'count': count})
+
+        image_urls = []
+        for image in images:
+            hd_image_url = image.get('hdurl')
+            if hd_image_url:
+                image_urls.append(hd_image_url)
+        return image_urls
 
 
-if __name__ == '__main__':
-    load_dotenv()
-    nasa_token = os.getenv('NASA_API_KEY')
-
-    nasa_instance = NasaAPI(token=nasa_token)
-    apod = nasa_instance.get_apod()
+def get_image_extension(url):
+    return os.path.splitext(url)[1]

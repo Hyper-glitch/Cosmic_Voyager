284ba5f65e09bec7a552845f604e5dfb4f1b23ba
Roman <graff049@gmail.com>
2022-04-13 22:36:33 +0300

    1. publish text to Telegram channel

---
+++ telegram_bot.py
@@ -0,0 +1,19 @@
+import os
+
+import telegram
+from dotenv import load_dotenv
+
+
+def init_telegram_bot(telegram_token):
+    return telegram.Bot(token=telegram_token)
+
+
+if __name__ == '__main__':
+    load_dotenv()
+
+    # chat_id канала — это ссылка на него, например: @cosmic_odyssey_2022
+    chat_id = os.environ.get('CHAT_ID')
+    telegram_token = os.getenv('TG_TOKEN')
+
+    tg_bot = init_telegram_bot(telegram_token=telegram_token)
+    tg_bot.send_message(text='Hello universe!', chat_id=chat_id)

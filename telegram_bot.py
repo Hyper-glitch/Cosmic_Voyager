2823a90babbee38a1f8749c45053d30a692bb548
Roman <graff049@gmail.com>
2022-04-13 22:58:21 +0300

    1. start to make logic for getting all images from all dirs ans subdirs

--- telegram_bot.py
+++ telegram_bot.py
@@ -13,7 +13,13 @@ if __name__ == '__main__':
 
     # chat_id канала — это ссылка на него, например: @cosmic_odyssey_2022
     chat_id = os.environ.get('CHAT_ID')
+    send_photo_period = os.getenv('SEND_PHOTO_PERIOD')
     telegram_token = os.getenv('TG_TOKEN')
 
     tg_bot = init_telegram_bot(telegram_token=telegram_token)
-    tg_bot.send_photo(chat_id=chat_id, photo=open('images/nasa/EPIC/nasa0.png', 'rb'))
+
+    dir_path = 'images/nasa/'
+    dirs = os.walk(dir_path)
+    while True:
+        for dir in dirs:
+            tg_bot.send_photo(chat_id=chat_id, photo=open(dir_path, 'rb'))

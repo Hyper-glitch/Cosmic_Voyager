6035e8846d86d91720d6261816ef813b46f1425a
Roman <graff049@gmail.com>
2022-04-14 20:27:33 +0300

    1. bot sends photos every 10 sec

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,10 @@
 import os
+import time
 
 import telegram
 from dotenv import load_dotenv
+
+from scraper_utils import get_all_image_paths
 
 
 def init_telegram_bot(telegram_token):
@@ -13,13 +16,14 @@ if __name__ == '__main__':
 
     # chat_id канала — это ссылка на него, например: @cosmic_odyssey_2022
     chat_id = os.environ.get('CHAT_ID')
-    send_photo_period = os.getenv('SEND_PHOTO_PERIOD')
+    send_photo_period = float(os.getenv('SEND_PHOTO_PERIOD'))
     telegram_token = os.getenv('TG_TOKEN')
 
     tg_bot = init_telegram_bot(telegram_token=telegram_token)
 
-    dir_path = 'images/nasa/'
-    dirs = os.walk(dir_path)
-    while True:
-        for dir in dirs:
-            tg_bot.send_photo(chat_id=chat_id, photo=open(dir_path, 'rb'))
+    dir_path = 'images'
+    image_paths = get_all_image_paths(dir_path=dir_path)
+
+    for image_path in image_paths:
+        tg_bot.send_photo(chat_id=chat_id, photo=open(image_path, 'rb'))
+        time.sleep(send_photo_period)

c65da09d15e231134457cfbedc73f7f54e065764
Roman <graff049@gmail.com>
2022-04-17 19:39:06 +0300

    1. add docstrings for all functions

--- universe_scraper.py
+++ universe_scraper.py
@@ -6,7 +6,7 @@ import telegram
 from dotenv import load_dotenv
 
 from cosmic_APIs import SpaceXAPI, NasaAPI
-from scraper_utils import make_images_dir, save_images, get_images_content, get_all_image_paths
+from scraper_utils import make_images_dir, save_images_content, get_images_content, get_all_image_paths
 
 PATH_TO_SAVE_IMAGES = 'images/'
 HEADERS = {
@@ -16,7 +16,7 @@ HEADERS = {
 
 
 def run_spacex_scraper():
-
+    """The main logic for running spacex scraper, that scrapes SpaceX API."""
     scraper_name = 'spacex'
     base_url = 'https://api.spacexdata.com/v4/'
     dir_path = os.path.join(PATH_TO_SAVE_IMAGES, scraper_name)
@@ -31,11 +31,11 @@ def run_spacex_scraper():
         image_urls = space_x_instance.get_latest_launch_with_images()
 
     images_content = asyncio.run(get_images_content(image_urls=image_urls))
-    save_images(dir_path=dir_path, images_content=images_content, image_name=scraper_name)
+    save_images_content(dir_path=dir_path, images_content=images_content, image_name=scraper_name)
 
 
 def run_nasa_scraper(nasa_token):
-
+    """The main logic for running nasa scraper, that scrapes Nasa API."""
     scraper_name = 'nasa'
     base_url = 'https://api.nasa.gov/'
     params = {'api_key': nasa_token}
@@ -44,16 +44,16 @@ def run_nasa_scraper(nasa_token):
 
     nasa_instance = NasaAPI(base_url=base_url, headers=HEADERS, params=params)
 
-    for subdir in sub_dirs:
-        if subdir == 'APOD':
+    for sub_dir in sub_dirs:
+        if sub_dir == 'APOD':
             image_type = None
-        elif subdir == 'EPIC':
+        elif sub_dir == 'EPIC':
             image_type = 'png'
-        nasa_instance.save_images(dir_path=dir_path, image_name=scraper_name, subdir=subdir, image_type=image_type)
+        nasa_instance.save_images(dir_path=dir_path, image_name=scraper_name, subdir=sub_dir, image_type=image_type)
 
 
 def run_telegram_bot(telegram_token, send_photo_period, chat_id):
-
+    """The main logic for running telegram bot, that sends cosmic images into the channel."""
     tg_bot = telegram.Bot(token=telegram_token)
     image_paths = get_all_image_paths(dir_path=PATH_TO_SAVE_IMAGES)
 
@@ -63,6 +63,7 @@ def run_telegram_bot(telegram_token, send_photo_period, chat_id):
 
 
 def run_universe_scraper():
+    """The main logic for running the whole program."""
     load_dotenv()
     chat_id = os.environ.get('CHAT_ID')
     send_photo_period = float(os.getenv('SEND_PHOTO_PERIOD'))

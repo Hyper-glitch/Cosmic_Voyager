80161bf6cbc885c39b17f4bdd2a4eb44a97125c5
Roman <graff049@gmail.com>
2022-04-17 19:29:27 +0300

    1. add docstrings for all functions

--- scraper_utils.py
+++ scraper_utils.py
@@ -1,15 +1,23 @@
 import asyncio
 import os
 import pathlib
+from typing import List, Dict
 
 import aiohttp
 
 
-def make_images_dir(dir_path):
+def make_images_dir(dir_path: str):
+    """Creates directory for saving images and check if its already exists.
+    :param dir_path: - path for creating directory.
+    """
     pathlib.Path(dir_path).mkdir(parents=True, exist_ok=True)
 
 
-def get_all_image_paths(dir_path):
+def get_all_image_paths(dir_path: str) -> List:
+    """Walks through all directories and files in there Collect all paths for images.
+    :param dir_path: - path for creating directory.
+    :returns: image_paths - all paths for images.
+    """
     dirs = []
     image_paths = []
 
@@ -23,7 +31,12 @@ def get_all_image_paths(dir_path):
     return image_paths
 
 
-def save_images(dir_path, images_content, image_name):
+def save_images_content(dir_path: str, images_content: List, image_name: str):
+    """Saves images byte content into image file with extension and its name.
+    :param dir_path: - path for creating directory.
+    :param images_content: - content that gets from async responses.
+    :param image_name: - equal the scraper's name.
+    """
     for index, image_content in enumerate(images_content):
         filename = f"{image_name}{index}{image_content['image_extension']}"
         save_path = os.path.join(dir_path, filename)
@@ -32,7 +45,13 @@ def save_images(dir_path, images_content, image_name):
             image.write(image_content['content'])
 
 
-async def fetch(image_url, session, params=None):
+async def fetch(image_url, session, params=None) -> Dict:
+    """Sends async get requests for getting images byte content.
+    :param image_url: - url for getting image.
+    :param session: - session object from aiohttp package.
+    :param params: - additional info for getting response if it needs.
+    :returns: image_content: - dict with content and image's extension.
+    """
     image_content = {}
     async with session.get(image_url, params=params) as response:
         response.raise_for_status()
@@ -42,7 +61,12 @@ async def fetch(image_url, session, params=None):
         return image_content
 
 
-async def get_images_content(image_urls, params=None):
+async def get_images_content(image_urls: List, params: Dict = None):
+    """Creates tasks for async getting image's content.
+    :param image_urls: - urls for getting images.
+    :param params: - additional info for getting response if it needs.
+    :returns: images_content: - list of dicts with content and image's extension.
+    """
     tasks = []
     async with aiohttp.ClientSession() as session:
         for image_url in image_urls:
@@ -51,5 +75,9 @@ async def get_images_content(image_urls, params=None):
         return await images_content
 
 
-def get_image_extension(url):
+def get_image_extension(url: str) -> str:
+    """Gets image's extension from url.
+    :param url: - urls for getting images.
+    :returns: image extension.
+    """
     return os.path.splitext(url)[1]
